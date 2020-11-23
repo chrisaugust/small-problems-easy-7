433a3eb967b09859d53c8fe067cd7bcbf08bc0da
Chris Danielson <christopher.a.danielson@protonmail.com>
2020-11-23 15:48:09 -0700

    Add solution for 'Multiply Lists'

---
+++ multiply_lists.rb
@@ -0,0 +1,42 @@
+# Multiply Lists
+#
+# Problem
+# Write a method that takes two array arguments of equal lengths
+# and which contains only number elements, and returns an array
+# whose elements are the product of the pair of numbers at the same
+# index in the argument arrays. 
+#
+# Examples
+# multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
+#
+#
+# Data Structures / Type Signature
+# array -> array
+#
+#
+# Algorithm
+# 1) initialize an empty array (results_array)
+# 2) iterate over one of the argument arrays
+# 3) for each index value, multiply the associated elements
+#    from the argument arrays, appending the result to 
+#    results_array
+# 4) return results_array
+#
+# Code
+require 'test/unit'
+
+class MultiplyLists < Test::Unit::TestCase
+  def test_multiply_lists
+    assert_equal(multiply_list([3, 5, 7], [9, 10, 11]), [27, 50, 77])
+  end
+end
+
+def multiply_list(list1, list2)
+  results_array = []
+  
+  list1.each_with_index do |elem, idx|
+    results_array << elem * list2[idx]
+  end
+
+  results_array
+end
